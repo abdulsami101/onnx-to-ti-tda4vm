4cec2cc0596218596b42d6febdf50d9f7cb10aee
deltax <abdulsamimahar001@gmail.com>
2026-02-09 11:46:19 +0900

    checking model nodes

--- check.py
+++ check.py
@@ -1,6 +1,6 @@
 import onnx
 
-m = onnx.load("/home/edgeai/code/edgeai-benchmark/pha2_models/keypoints/v1/0919_384_640.onnx")
+m = onnx.load("models/detection/icms1/w_sami.onnx")
 
 print("Graph outputs:")
 for o in m.graph.output:
